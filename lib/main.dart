e39757d6148be0036eda6459ed876f4074f3e7b4
lukaswde <mail@lukasw.de>
2025-01-17 00:00:11 +0100

    Auth Pages etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
+import 'package:auth_firebase/firebase_options.dart';
+import 'package:auth_firebase/pages/login/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase/firebase_options.dart';
+import 'pages/signup/signup.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Login());
   }
 }
